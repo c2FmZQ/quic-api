63f5ccb9786a8101a8475e479a6b3be31825379f
Robin Thellend <rthellend@gmail.com>
2025-07-15 12:24:48 -0700

    Add import

--- update-code.sh
+++ update-code.sh
@@ -1,9 +1,28 @@
-#!/bin/bash
+#!/bin/bash -e
 # This scripts updates the go files in the current directory to use the API from
-# this package. It doesn't add the required import.
+# this package.
 
 for f in $(find . -name "*.go"); do
   sed -i -r \
-    -e 's:[*]quic[.]((Conn|(Send|Receive)?Stream|Transport)([,;\) ]|$)):quicapi.\1:' \
-    -e 's:quic[.]((Dial|Listen)(Addr)?(Early)?):quicapi.\1:' $f
+    -e 's:&quic[.]((Early)?Listener)\{\}:(quicapi.\1)(nil):' \
+    -e 's:[*]quic[.]((Conn|(Send|Receive)?Stream|Transport|EarlyListener)([,;\) ]|$)):quicapi.\1:' \
+    -e 's:quic[.]((Dial|Listen)(Addr)?(Early)?):quicapi.\1:' \
+    $f
+done
+
+for f in $(git grep -l quicapi); do
+  awk '/^import/ {
+        imp=1;
+      }
+      /^)?$/ {
+        if(imp) {
+          printf("\n\tquicapi \"github.com/c2FmZQ/quic-api\"\n")};
+          imp=0;
+        }
+      !/github.com\/c2FmZQ\/quic-api/ {
+        print;
+      }
+  ' < $f > tmpfile
+  mv tmpfile $f
+  gofmt -w $f
 done
